c528c2713afbf5fbd73034e401e4697dbbbbbb15
jc14745 <jc14745@CEIT2553214X005.ad.georgiasouthern.edu>
2018-09-12 17:16:54 -0400

    luggage added to hatchback

--- InheritanceExamples/src/Examples/HatchBack.java
+++ InheritanceExamples/src/Examples/HatchBack.java
@@ -1,8 +1,16 @@
 package Examples;
 
 public class HatchBack extends Car{
-	public HatchBack () {
-		super("Ford" , "Escape" , "blue");
+	private int luggageNum;
+	public HatchBack (String make, String model, String color, int numOfLuggageInBack) {
+		super(make, model, color);
+		this.luggageNum = numOfLuggageInBack;
+	}
+	public int getLuggageNum() {
+		return luggageNum;
+	}
+	public void setLuggageNum(int luggageNum) {
+		this.luggageNum = luggageNum;
 	}
 
 }
--- InheritanceExamples/src/Examples/Vehicles.java
+++ InheritanceExamples/src/Examples/Vehicles.java
@@ -4,7 +4,7 @@ public class Vehicles {
 	public static void main(String [] args) {
 	Car c1 = new Car("Ford", "Escape", "Purple");
 	System.out.println(c1);
-	HatchBack h1 = new HatchBack();
+	HatchBack h1 = new HatchBack("Ford", "Escape", "blue", 5);
 	System.out.println("My brand new hatchback " + h1);
 	}
 }
